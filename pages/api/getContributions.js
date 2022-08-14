91806966ecb090f5f3de69e7a31a588840419bc0
GLTcode <88385626+GLTdotNS@users.noreply.github.com>
2022-08-15 00:27:44 +0300

    fix bugs :(

--- pages/api/getContributions.js
+++ pages/api/getContributions.js
@@ -1,7 +1,7 @@
 export default function handler (req, res) {
     const headers = {
 
-      'Authorization':"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",
+      'Authorization':"bearer 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",
     }
     const body = { "query": "query {viewer {contributionsCollection {contributionCalendar {totalContributions}}}}" }
      fetch('https://api.github.com/graphql', {
