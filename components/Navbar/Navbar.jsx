eb6f0afa0ee11a80dc1847140fbdde2a896424b0
GLTcode <88385626+GLTdotNS@users.noreply.github.com>
2022-08-28 09:50:27 +0300

    change navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineClose } from 'react-icons/ai';
 import Link from "next/link"
 import { motion, AnimatePresence, useScroll } from 'framer-motion';
-import { SiFacebook, SiGithub, SiGmail } from 'react-icons/si';
+
 
 
 
@@ -16,10 +16,10 @@ const Navbar = () => {
 
   const update = () => {
 
- 
+
     if (scrollY?.current < scrollY?.prev) {
       setHidden(false);
- 
+
 
     } else if (scrollY?.current > 1 && scrollY?.current > scrollY?.prev) {
 
@@ -41,16 +41,8 @@ const Navbar = () => {
 
   const variants = {
 
-    visible: { opacity: 1, y: 0,backgroundColor: "#161b22", },
+    visible: { opacity: 1, y: 0, backgroundColor: "#161b22", },
     hidden: { opacity: 0, y: -25 }
-  };
-
-  const itemVariants = {
-    closed: {
-      visibility: "hidden",
-    
-    },
-    open: { delay: 1, visibility: "visible"  }
   };
 
 
@@ -83,10 +75,10 @@ const Navbar = () => {
       animate={hidden ? "hidden" : "visible"}
       transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
     >
-      
-      { !toggleMenu ? <GiHamburgerMenu style={{transition: "0.5s"}} color="#fff" cursor={"pointer"}
-          fontSize={27} onClick={() => setToggleMenu(true)} /> :
-           <AiOutlineClose fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />}
+
+      {!toggleMenu ? <GiHamburgerMenu style={{ transition: "0.5s" }} color="#fff" cursor={"pointer"}
+        fontSize={27} onClick={() => setToggleMenu(true)} /> :
+        <AiOutlineClose fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />}
 
       <div className="app__navbar-logo " onClick={() => setToggleMenu(false)}>
         <Link href="/">
@@ -99,75 +91,51 @@ const Navbar = () => {
 
         <AnimatePresence>
 
-        {toggleMenu && (
-        <motion.div className='k'>
-            <motion.div className="app__navbar-smallscreen_overlay"
-            
-            transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
-            animate={{
-              width: "285px",
-              transition: { duration: 0 }
-            }}
-            exit={{
-              width: 0,
-              transition: { delay: 0.1, duration: 0.6 }
-            }}
-            >
-          
-             
+          {toggleMenu && (
+            <div className='k'>
+              <motion.div className="app__navbar-smallscreen_overlay"
 
-              <motion.ul className="app__navbar-smallscreen_links"
-                initial="closed"
-                animate="open"
-                exit="closed"
-                variants={sideVariants}
+                animate={{
+                  width: "285px",
+
+                }}
               >
 
-                <motion.li
-                  variants={itemVariants}
-                  className="p__opensans"
-                  onClick={() => setToggleMenu(false)}>
-
-                  <Link href="/">Blog</Link>
-                </motion.li>
-
-                <motion.li
-                  variants={itemVariants} className="p__opensans"
-                  onClick={() => setToggleMenu(false)}>
-
-                  <Link href="/aboutme">About me</Link>
-                </motion.li>
-
-                <motion.li
-                  variants={itemVariants} className="p__opensans"
-                  onClick={() => setToggleMenu(false)}>
-
-                  <Link href="/projects">Projects</Link>
-                </motion.li>
 
 
-              </motion.ul>
+                <ul className="app__navbar-smallscreen_links"
 
-              <motion.div
-                initial="closed"
-                animate="open"
-                exit="closed"
-                variants={sideVariants}>
-                <hr />
-                <div className="app__navbar-logo nav-social">
-     
-                </div>
+                >
 
-                <div className="nav-social">
-                  <motion.a variants={itemVariants} href="https://www.facebook.com/" className="facebook"><SiFacebook id='footer_facebook' /></motion.a>
-                  <motion.a variants={itemVariants} href="https://github.com/GLTdotNS" className="github"><SiGithub id='footer_github' /></motion.a>
-                  <motion.a variants={itemVariants} href="mailto:georgitonkow@gmail.com" className="google"><SiGmail /></motion.a>
-                </div>
+                  <li
+
+                    className="p__opensans"
+                    onClick={() => setToggleMenu(false)}>
+
+                    <Link href="/">Blog</Link>
+                  </li>
+
+                  <li
+                    className="p__opensans"
+                    onClick={() => setToggleMenu(false)}>
+
+                    <Link href="/aboutme">About</Link>
+                  </li>
+
+                  <li
+                    className="p__opensans"
+                    onClick={() => setToggleMenu(false)}>
+
+                    <Link href="/projects">Projects</Link>
+                  </li>
+
+
+                </ul>
+                  <hr />
               </motion.div>
-            </motion.div>
 
 
-        </motion.div>
+            </div>
           )}
 
         </AnimatePresence>
