bafbef08331fb5e724bddf7f1d0db60e80aa39f9
GLTdotNS <georgitonkov94@gmail.com>
2022-10-10 15:05:53 +0300

    change navbar and logo

--- components/Blog/BlogPageComponents/PostsComponent.jsx
+++ components/Blog/BlogPageComponents/PostsComponent.jsx
@@ -1,97 +1,93 @@
-import React from 'react'
+import React from "react";
 import moment from "moment";
-import { MdReadMore } from "react-icons/md"
+import { MdReadMore } from "react-icons/md";
 import { urlForImg } from "../../../lib/sanityClient.js";
-import Link from "next/link"
-import { useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import Link from "next/link";
+import { useState } from "react";
+import ReactPaginate from "react-paginate";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const PostsComponent = ({ posts }) => {
-
-  const [pageNumber, setPageNumber] = useState(0)
+  const [pageNumber, setPageNumber] = useState(0);
   const postPerPage = 3;
   const pageVisited = pageNumber * postPerPage;
 
+  const displayPost =
+    posts &&
+    posts.slice(pageVisited, pageVisited + postPerPage).map((post, index) => {
+      let wordsCount = post.description.split(" ");
 
+      let minutesToRead = wordsCount.length / 200;
 
-  const displayPost = posts && posts.slice(pageVisited, pageVisited + postPerPage).map((post, index) => {
+      if (!post) {
+        return (
+          <SkeletonTheme baseColor="#202020" highlightColor="#444">
+            <div className="card initial-post">
+              <h3>
+                <Skeleton width={50} />
+              </h3>
+              <h5>
+                <Skeleton width={80} />
+              </h5>
 
-    let wordsCount = post.description.split(' ')
-
-    let minutesToRead =  wordsCount.length / 200;
-
-
-    if (!post) {
-      return (
-
-        <SkeletonTheme baseColor="#202020" highlightColor="#444">
-
-          <div className='card initial-post'>
-            
-            <h3><Skeleton width={50} /></h3>
-            <h5><Skeleton width={80} /></h5>
-
-            <div className='inner_post_text'>
-              <span>
-                <Skeleton height={100} />
-              </span>
+              <div className="inner_post_text">
+                <span>
+                  <Skeleton height={100} />
+                </span>
+              </div>
             </div>
+          </SkeletonTheme>
+        );
+      }
 
+      return (
+        <div className=" initial-post" key={index}>
+          <h3 style={{ marginBottom: "4px" }}>{post.title}</h3>
+          <p>
+            {post.publishedAt
+              ? moment(post.publishedAt).format("YYYY , MMM  DD , HH:MM")
+              : "YYYY-MM-DD hh:mm"}
+          </p>
+          <p>{minutesToRead.toFixed(0)} minutes to read</p>
+          <div className="inner_post_text">
+            {post.mainImage ? (
+              <img
+                loading="lazy"
+                style={{
+                  float: "left",
+                  margin: "0 15px 0 0",
+                  padding: "1%",
+                  borderRadius: "30%",
+                }}
+                src={urlForImg(post.mainImage.asset.url)}
+                alt="Image of the post"
+                width="150"
+                height="150"
+              />
+            ) : (
+              "none"
+            )}
+
+            <span> {post?.description.slice(0, 300)}...</span>
           </div>
-
-        </SkeletonTheme>
-
-      )
-    }
- 
-    return (
-
-
-      <div className=" initial-post" key={index}
-
-      >
-        <h3 style={{marginBottom: "4px"}}>{post.title}</h3>
-        <p>{post.publishedAt ? moment(post.publishedAt).format("YYYY , MMM  DD , HH:MM")
-          : "YYYY-MM-DD hh:mm"}</p>
-    <p>{minutesToRead.toFixed(0)} minutes to read</p>
-
-        <div className='inner_post_text'>
-
-          {post.mainImage ? <img
-          
-            loading='lazy'
-            style={{
-
-              float: "left", margin: "0 15px 0 0",
-              padding: "1%", borderRadius: "30%"
-            }}
-            src={urlForImg(post.mainImage.asset.url)}
-            alt="Image of the post" width="150" height="150" /> : "none"}
-
-          <span> {post?.description.slice(0, 300)}...</span>
-
-
-
+          <Link href="/post/[slug]" as={`/post/${post.slug.current}`}>
+            <a>
+              <button className="btn">
+                Read more <MdReadMore />
+              </button>
+            </a>
+          </Link>{" "}
         </div>
-
-        <Link href="/post/[slug]" as={`/post/${post.slug.current}`}>
-          <a><button className="btn">Read more <MdReadMore /></button></a>
-        </Link>{' '}
-      </div>
-
-
-    )
-
-  })
+      );
+    });
 
   const pageCount = posts && Math.ceil(posts.length / postPerPage);
 
   const handleChange = ({ selected }) => {
     setPageNumber(selected);
-    window.scrollTo(0, 0)
-  }
+    window.scrollTo(0, 0);
+  };
 
   return (
     <>
@@ -107,10 +103,9 @@ const PostsComponent = ({ posts }) => {
         marginPagesDisplayed={2}
         pageCount={pageCount}
         onPageChange={handleChange}
-
       />
     </>
-  )
-}
+  );
+};
 
-export default PostsComponent
+export default PostsComponent;
--- components/Blog/BlogPageComponents/RecentlyPosts.jsx
+++ components/Blog/BlogPageComponents/RecentlyPosts.jsx
@@ -1,26 +1,32 @@
-import moment from 'moment';
-import React from 'react'
-import Link from "next/link"
+import moment from "moment";
+import React from "react";
+import Link from "next/link";
+import { urlForImg } from "../../../lib/sanityClient.js";
+
 const RecentlyPosts = ({ posts }) => {
-
   return (
-    <div className="columns posts" >
-
+    <div className="columns posts">
       <h3 className="p__opensans title">Recently Posts</h3>
       <hr />
-      <ol className="section" >
-        {posts && posts.slice(0, 3).map((post, index) =>
-
-          <li key={index}>
-            {moment(post.publishedAt).format("MMM DD")}
-            <Link href="/post/[slug]" as={`/post/${post.slug.current}`}
-            ><a>{post.title}</a></Link>
-            <hr />
-          </li>
-        )}
+      <ol className="section">
+        {posts &&
+          posts.slice(0, 3).map((post, index) => (
+            <li key={index}>
+              {moment(post.publishedAt).format("MMM/DD")}
+              <Link href="/post/[slug]" as={`/post/${post.slug.current}`}>
+                <a>{post.title}</a>
+              </Link>
+              <img
+                width={50}
+                height={80}
+                src={urlForImg(post.mainImage.asset.url)}
+              />
+              <hr />
+            </li>
+          ))}
       </ol>
     </div>
-  )
-}
+  );
+};
 
-export default RecentlyPosts
+export default RecentlyPosts;
