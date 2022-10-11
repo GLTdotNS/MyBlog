f53626a58706a2263f1680850ab89b0eabc389de
GLTdotNS <georgitonkov94@gmail.com>
2022-10-11 14:14:08 +0300

    add files

--- components/Cookies/Cookies.jsx
+++ components/Cookies/Cookies.jsx
@@ -5,24 +5,24 @@ const Cookies = () => {
   return (
     <div
       data-nosnippet=""
-      class="jetbrains-cookies-banner jetbrains-cookies-banner--position-right"
+      className="jetbrains-cookies-banner jetbrains-cookies-banner--position-right"
     >
-      <div class="jetbrains-cookies-banner__body">
-        <div class="jetbrains-cookies-banner__paragraph">
+      <div className="jetbrains-cookies-banner__body">
+        <div className="jetbrains-cookies-banner__paragraph">
           Cookies and IP addresses allow us to deliver and improve our web
           content and to provide you with a personalized experience. Our website
           uses cookies and collects your IP address for these purposes.{" "}
         </div>
 
-        <div class="jetbrains-cookies-banner__paragraph">
-          <div class="jetbrains-cookies-banner__pre">
-            <div class="jetbrains-cookies-banner__pre-inner">
+        <div className="jetbrains-cookies-banner__paragraph">
+          <div className="jetbrains-cookies-banner__pre">
+            <div className="jetbrains-cookies-banner__pre-inner">
               {" "}
               JetBrains may use cookies and my IP address to
               <br /> collect individual statistics and to provide me with
               <br /> personalized offers and ads subject to the{" "}
               <a
-                class="jetbrains-cookies-banner__link"
+                className="jetbrains-cookies-banner__link"
                 href="https://www.jetbrains.com/company/privacy.html"
               >
                 Privacy
@@ -30,7 +30,7 @@ const Cookies = () => {
               </a>{" "}
               and the{" "}
               <a
-                class="jetbrains-cookies-banner__link"
+                className="jetbrains-cookies-banner__link"
                 href="https://www.jetbrains.com/company/useterms.html"
               >
                 Terms of Use
@@ -38,7 +38,7 @@ const Cookies = () => {
               . JetBrains may use
               <br />
               <a
-                class="jetbrains-cookies-banner__link"
+                className="jetbrains-cookies-banner__link"
                 href="https://www.jetbrains.com/company/privacy.html"
               >
                 third-party services
@@ -46,22 +46,22 @@ const Cookies = () => {
               for this purpose. I can revoke
               <br /> my consent at any time by visiting the{" "}
               <a
-                class="jetbrains-cookies-banner__link"
+                className="jetbrains-cookies-banner__link"
                 href="https://www.jetbrains.com/opt-out/"
               >
                 Opt-Out page
               </a>
               .<br />
               <br />
-              <div class="jetbrains-cookies-banner__buttons ">
+              <div className="jetbrains-cookies-banner__buttons ">
                 <a
-                  class="jetbrains-cookies-banner__choice-link"
+                  className="jetbrains-cookies-banner__choice-link"
                   data-choice="yes"
                 >
                   [<b>Y</b>]es, I agree
                 </a>
                 <a
-                  class="jetbrains-cookies-banner__choice-link"
+                  className="jetbrains-cookies-banner__choice-link"
                   data-choice="no"
                 >
                   [<b>N</b>]o, thanks
@@ -71,16 +71,16 @@ const Cookies = () => {
           </div>
         </div>
 
-        <div class="jquery-console-inner jquery-console-nofocus">
-          <div class="jquery-console-prompt-box">
-            <span class="jquery-console-prompt-label">~&nbsp;root# </span>
-            <span class="jquery-console-prompt">
-              <span class="jquery-console-cursor">&nbsp;</span>
+        <div className="jquery-console-inner jquery-console-nofocus">
+          <div className="jquery-console-prompt-box">
+            <span className="jquery-console-prompt-label">~&nbsp;root# </span>
+            <span className="jquery-console-prompt">
+              <span className="jquery-console-cursor">&nbsp;</span>
             </span>
           </div>
         </div>
       </div>
-      <div class="jetbrains-cookies-banner__close-button">[X]</div>
+      <div className="jetbrains-cookies-banner__close-button">[X]</div>
     </div>
   );
 };
