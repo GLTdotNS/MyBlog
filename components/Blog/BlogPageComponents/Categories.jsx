bbf0189ceca37a92b87cc70e667492ff6b8cae8d
GLTdotNS <georgitonkov94@gmail.com>
2022-11-18 16:35:08 +0200

    SEO bugs

--- components/Blog/BlogPageComponents/Categories.jsx
+++ components/Blog/BlogPageComponents/Categories.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
 const Categories = ({ category }) => {
-
   if (!category) {
-    return <p>loading</p>
+    return <p>loading</p>;
   }
 
   return (
-    <div className="categories columns" style={{ backgroundColor: "#333", borderRadius: "10px" }}>
-      <h3 className="p__opensans title">Categories</h3>
-      <hr />
-      {category && category.map((category, index) =>
-
-
-        <span key={index} >
-          {!category ? "Something went wrong..." :
-            <Link href={"/categories/[slug]"} as={`/categories/${category.slug.current}`}>
-              <a> {category.title}</a>
-            </Link>
-
-          }
-        </span>
-
-
-      )}
+    <div
+      className="categories  columns"
+      style={{ backgroundColor: "#333", borderRadius: "10px" }}
+    >
+      <div className="dropdown ">
+        <h3 className="p__opensans  title">Categories</h3>
+        <hr />
+        {category &&
+          category.map((category, index) => (
+            <span className="dropdown-content" key={index}>
+              {!category ? (
+                "Something went wrong..."
+              ) : (
+                <Link
+                  href={"/categories/[slug]"}
+                  as={`/categories/${category.slug.current}`}
+                >
+                  <a> {category.title}</a>
+                </Link>
+              )}
+            </span>
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
