20ca5d63586ebfdc5d418433d84534341fcf5fb4
GLTdotNS <georgitonkov94@gmail.com>
2022-12-05 00:26:27 +0200

    add files

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,20 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <>
+      <Head>
+        <title>NONCREATIVEBLOG</title>
+        <meta
+          name="description"
+          content="portfolio ☝️  This is a dev blog about the projects I'm working on at the moment. 
+          The blog includes different kinds of articles related to the projects and links to the projects' Github repositories.
+          This is a blog post that I made to share some of my experience with React, React NextJS and a little bit of web development.
+          My portfolio  is just a collection of a few of the projects that I've worked on. It showcases the different areas of web development that I enjoy.
+          React is one of the most popular JavaScript frameworks for creating web applications. It uses Virtual DOM for memory management and is fully reactive."
+          key="desc"
+        />
+        <meta name="yandex-verification" content="c1ca0e0d1f097644" />
+      </Head>
+
       <Toaster />
       <Component {...pageProps} />
     </>
