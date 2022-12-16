361cd5bea3b1fa41737606c0149d8c40c0c45443
GLTdotNS <georgitonkov94@gmail.com>
2022-12-16 21:53:25 +0200

    delete some things

---
+++ pages/api/contact.js
@@ -0,0 +1,33 @@
+export default async function (req, res) {
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "georgitonkow@gmail.com",
+      pass: process.env.NEXT_PUBLIC_GMAIL.replace(/\\n/g, "\n"),
+    },
+    secure: true,
+  });
+  const mailData = {
+    from: "georgitonkow@gmail.com",
+    to: "georgitonkov94@gmail.com",
+    subject: `Message From ${req.body.name}`,
+    text:
+      req.body.message +
+      " | Sent from: " +
+      req.body.email +
+      " Company: " +
+      req.body.company,
+    html: `<div>${req.body.message}</div><p>Sent from:
+    ${req.body.email}</p>`,
+  };
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) console.log(err);
+      else console.log(info);
+      res.status(200).json({ data: res.status });
+    });
+  });
+}
