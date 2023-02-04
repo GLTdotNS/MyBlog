7c2b3ab8190e4b5a5a0c145cc9d1c84f0d07f30e
GLTdotNS <georgitonkov94@gmail.com>
2023-02-04 17:19:26 +0200

    delete

--- components/About/Hero/Skills.jsx
+++ components/About/Hero/Skills.jsx
@@ -1,54 +1,63 @@
-import React, { useEffect } from 'react'
-import { IoLogoCss3, IoLogoHtml5 } from 'react-icons/io'
-import { GrReactjs } from 'react-icons/gr'
-import { SiJavascript, SiCsharp } from 'react-icons/si'
-import { reveal } from '../../../animations/skillsBarAnimation'
+import React, { useEffect } from "react";
+import { IoLogoCss3, IoLogoHtml5 } from "react-icons/io";
+import { GrReactjs } from "react-icons/gr";
+import { SiJavascript, SiCsharp } from "react-icons/si";
+import { reveal } from "../../../animations/skillsBarAnimation";
 
 const Skills = () => {
-
   useEffect(() => {
     window.onload = reveal();
     window.addEventListener("scroll", reveal);
-  },[])
-
+  }, []);
 
   return (
-    <div className='Skilss'>
-      <div className='skills-section '>
-      <h2>Tech skills</h2>
+    <div className="Skilss">
+      <div className="skills-section ">
+        <h2>Tech skills</h2>
 
+        <p>
+          {" "}
+          <IoLogoHtml5 size={20} color={"red"} /> HTML
+        </p>
+        <div className="container">
+          <div id="html" className="skills ">
+            90%
+          </div>
+        </div>
 
-      <p > <IoLogoHtml5 size={20} color={"red"}  /> HTML</p>
-      <div className="container">
-        <div id='html' className="skills ">90%</div>
+        <p>
+          {" "}
+          <IoLogoCss3 size={20} color={"blue"} /> CSS
+        </p>
+        <div className="container">
+          <div id="css" className="skills">
+            75%
+          </div>
+        </div>
+
+        <p>
+          <SiJavascript size={20} color={"yellow"} /> JavaScript
+        </p>
+        <div className="container">
+          <div id="js" className="skills ">
+            80%
+          </div>
+        </div>
+
+        <p>
+          {" "}
+          <SiCsharp size={20} color={"blue"} /> C Hashtag{" "}
+        </p>
+        <div className="container">
+          <div id="csharp" className="skills">
+            80%
+          </div>
+        </div>
+
+        <p></p>
       </div>
-
-
-      <p> <IoLogoCss3 size={20} color={"blue"} /> CSS</p>
-      <div className="container">
-        <div id='css' className="skills">75%</div>
-      </div>
-
-     
-      <p><SiJavascript size={20} color={"yellow"} /> JavaScript</p>
-      <div className="container">
-        <div id='js' className="skills ">80%</div>
-      </div>
-
-      <p> <SiCsharp size={20} color={"blue"} /> C Hashtag </p>
-      <div className="container">
-        <div id='csharp' className="skills">80%</div>
-      </div>
-
-      <p></p>
-      <p><GrReactjs size={20} color={"blue"} /> React JS </p>
-      <div className="container">
-        <div id='react' className="skills">Loading...</div>
-      </div>
-
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
