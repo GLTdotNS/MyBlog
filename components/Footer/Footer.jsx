fcaae6ab302cc760a8bc6bad24a2ae51cd8084c9
GLTdotNS <georgitonkov94@gmail.com>
2023-02-14 15:46:01 +0200

    initial commit

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -6,60 +6,29 @@ import { SiNextdotjs } from "react-icons/si";
 const Footer = () => {
   return (
     <div className="footer_div">
-      <div>
-        <footer>
-          <hr />
-
-          <div
-            style={{ backgroundColor: "#000324 !important" }}
-            className="row primary"
-          >
-            <div className="column about">
-              <h3>Georgi Tonkov</h3>
-              <ul>
-                <li>A Bad Day On The Mountain</li>
-                <li>Bike Always Beats A Good Day In The Office</li>
-              </ul>
-            </div>
-
-            <div className="column ">
-              <h3>
-                Made with <BsHeartFill size={20} color={"red"} />
-              </h3>
-              <ul>
-                <h3>
-                  {" "}
-                  Courtesy of <SiNextdotjs size={20} color={"white"} /> and{" "}
-                </h3>
-                <a href="https://www.sanity.io/" id="sanity">
-                  Sanity.io
-                </a>
-              </ul>
-            </div>
-            <div className="social">
-              <a href="https://github.com/GLTdotNS" className="github">
-                <SiGithub id="footer_github" />
-              </a>
-              <a href="mailto:georgitonkow@gmail.com" className="google">
-                <SiGmail />
-              </a>
-            </div>
+      <footer>
+        <div class="footer">
+          <div class="footerRow">
+            {" "}
+            <a href="https://github.com/GLTdotNS" className="github">
+              <SiGithub id="footer_github" />
+            </a>
           </div>
 
-          <div
-            className=" copyright"
-            style={{ backgroundColor: "#000324 !important" }}
-          >
-            <p id="open_source">
-              <a href="https://github.com/GLTdotNS/nextjs" className="links">
-                Open source system
-              </a>{" "}
-              <SiGithub size={10} />
-            </p>
-            <p>Copyright &copy; 2022 Tonkoff</p>
-          </div>
-        </footer>
-      </div>
+          <h3>
+            Made with <BsHeartFill size={20} color={"red"} />
+          </h3>
+          <ul>
+            <h3>
+              {" "}
+              Courtesy of <SiNextdotjs size={20} color={"white"} /> and{" "}
+            </h3>
+            <a href="https://www.sanity.io/" id="sanity">
+              Sanity.io
+            </a>
+          </ul>
+        </div>
+      </footer>
     </div>
   );
 };
