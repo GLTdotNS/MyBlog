46929611954da47aec13f5391040f3eed2cb2acf
GLTdotNS <georgitonkov94@gmail.com>
2023-03-01 21:05:58 +0200

    initial commit

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -339,7 +339,7 @@ export async function getStaticPaths() {
   const paths = posts.map((post) => ({ params: { slug: post.slug.current } }));
   return {
     paths,
-    fallback: "blocking",
+    fallback: false,
   };
 }
 export async function getStaticProps(context) {
