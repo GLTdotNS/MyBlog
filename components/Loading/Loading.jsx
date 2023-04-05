3559584d112a7b38916259080aaf1a1f6c534eff
GLTdotNS <georgitonkov94@gmail.com>
2023-04-05 22:20:50 +0300

    initial commit

--- components/Loading/Loading.jsx
+++ components/Loading/Loading.jsx
@@ -1,102 +1,22 @@
 import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import Layout from "../Layout/Layout";
-import logo from "../../styles/assets/a.jpg";
+import logo from "../../styles/assets/wolf.jpg";
 import Image from "next/image";
 const Loading = () => {
   return (
     <Layout>
-      <SkeletonTheme baseColor="#202124" highlightColor="#444">
-        <main className="">
-          <Skeleton width={"100%"} height={500} />
-          <div className="row">
-            <div className="midcolumn ">
-              <form className="box " style={{ marginTop: "10%" }}>
-                <h1 className="">
-                  <Skeleton />
-                </h1>
-
-                <div className="search">
-                  <Skeleton />
-                </div>
-              </form>
-              <hr />
-
-              <hr />
-              <div className=" initial-post">
-                <h3>
-                  <Skeleton width={50} />
-                </h3>
-                <h5>
-                  <Skeleton width={80} />
-                </h5>
-
-                <div className="inner_post_text">
-                  <span>
-                    <Skeleton height={100} />
-                  </span>
-                </div>
-              </div>
-              <div className=" initial-post">
-                <h3>
-                  <Skeleton width={50} />
-                </h3>
-                <h5>
-                  <Skeleton width={80} />
-                </h5>
-
-                <div className="inner_post_text">
-                  <span>
-                    <Skeleton height={100} />
-                  </span>
-                </div>
-              </div>
-              <div className=" initial-post">
-                <h3>
-                  <Skeleton width={50} />
-                </h3>
-                <h5>
-                  <Skeleton width={80} />
-                </h5>
-
-                <div className="inner_post_text">
-                  <span>
-                    <Skeleton height={100} />
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="rightcolumn">
-              <div
-                className="categories  columns"
-                style={{ backgroundColor: "#333", borderRadius: "10px" }}
-              >
-                <div className="dropdown ">
-                  <h3 className="p__opensans  title">
-                    <Skeleton />
-                  </h3>
-                  <hr />
-
-                  <span className="dropdown-content">
-                    <Skeleton height={80} />
-                  </span>
-                </div>
-              </div>
-              <div className="columns posts">
-                <h3 className="p__opensans title">
-                  <Skeleton />
-                </h3>
-                <hr />
-                <ul className="section ">
-                  <div style={{ cursor: "pointer" }}>
-                    <Skeleton height={80} />
-                  </div>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </main>
-      </SkeletonTheme>
+      <div class="loading">
+        <div class="loading-text">
+          <span class="loading-text-words">L</span>
+          <span class="loading-text-words">O</span>
+          <span class="loading-text-words">A</span>
+          <span class="loading-text-words">D</span>
+          <span class="loading-text-words">I</span>
+          <span class="loading-text-words">N</span>
+          <span class="loading-text-words">G</span>
+        </div>
+      </div>
     </Layout>
   );
 };
